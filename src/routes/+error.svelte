b2a7b48e877215ef233ee777f0599e69e83f76bf
levy <897895407@qq.com>
2023-12-25 00:27:29 +0800

    feat: custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+
+	async function clear() {
+    await fetch(`/api/settings`, {
+      method: 'DELETE',
+    });
+    location.reload()
+	}
+</script>
+
+<h1>{$page.status}: {$page.error.message}</h1>
+
+<button on:click={clear}>clear cache</button>
