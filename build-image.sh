c1a28a4e3a885ccbbc6c185b1f52d707d9d53d06
levy <897895407@qq.com>
2023-12-30 10:15:22 +0800

    ci: build and push in shell

--- build-image.sh
+++ build-image.sh
@@ -3,3 +3,30 @@
 #yarn && yarn build
 TAG=$(./get-version.sh)
 docker build -t levychen/chroma-viewer:$TAG .
+
+# --push
+push=false
+
+# Process command-line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --push)
+      push=true
+      ;;
+    *)
+      echo "Unknown argument will be ignored: $1"
+      ;;
+  esac
+  shift
+done
+
+if [ "$push" = true ]; then
+  echo "push is true. Performing push operation..."
+
+  echo "Pushing to registry..."
+  docker push levychen/chroma-viewer:$TAG
+
+  echo "Build and push complete!"
+else
+  echo "push is false. Skipping push operation."
+fi
